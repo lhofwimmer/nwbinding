77a37010152c8de6607fd486acd043d8f3390cd4
Andreas Zauner <andi4971@gmail.com>
2018-04-25 10:47:00 +0200

    supp

--- Northwindbinding/Controllers/SupplierController.cs
+++ Northwindbinding/Controllers/SupplierController.cs
@@ -3,14 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Northwindbinding.Models;
+using NorthwindLibrary;
 
 namespace Northwindbinding.Controllers
 {
     public class SupplierController : Controller
     {
-        public IActionResult Index()
+        NORTHWNDEntities db;
+        public SupplierController(NORTHWNDEntities db)
+        {
+            this.db = db;
+        }
+        [Route("Supplier/New")]
+        public IActionResult Create()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult HandleSupplier()
+        {
+            NewSupplierModel suppModel = new NewSupplierModel();
+            TryUpdateModelAsync(suppModel);
+            Suppliers s = new Suppliers();
+            s.City = suppModel.City;
+            s.CompanyName = suppModel.CompanyName;
+            s.ContactName = suppModel.ContactName;
+            if (suppModel.Country != null) s.Country = suppModel.Country;
+            if (suppModel.PostalCode != null) s.PostalCode = (suppModel.PostalCode??0)+"";
+            db.Suppliers.Add(s);
+            db.SaveChanges();
+            return View("Create");
         }
     }
 }
---
+++ Northwindbinding/Models/NewSupplierModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Northwindbinding.Models
+{
+    public class NewSupplierModel
+    {   [Required]
+        public String CompanyName { get; set; }
+        [Required]
+        public String ContactName { get; set; }
+        [Required]
+        public String City { get; set; }
+
+        public int? PostalCode { get; set; }
+        public String  Country { get; set; }
+    }
+}
